81603382de275328ed345c2789165e74db170bcb
fedeval98 <fede.val9806@gmail.com>
2024-03-19 07:00:06 -0300

    corriendo mas errores

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <template>
   <div class="">
-    <Header/>
-    <div class="flex flex-col gap-8 justify-center items-center"> 
-      <Redes/>
+    <Header />
+    <div class="flex flex-col gap-8 justify-center items-center">
+      <Redes />
     </div>
   </div>
 </template>
 
 <script setup>
-
-  import Redes from "./components/Redes.vue"
-  import Header from "./components/Header.vue"
-  import Particles from 'particlesjs'
-
+import Redes from './components/Redes.vue'
+import Header from './components/Header.vue'
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
