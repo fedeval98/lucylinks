824c6863632809c37edd7eb577e0ed0f74b40eae
fedeval98 <fede.val9806@gmail.com>
2024-04-17 01:33:15 -0300

    mejorando diseñox2

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ import './assets/style.css'
 window.onload = function () {
   Particles.init({
     selector:'.background',
-    color:['#D03958','#623C57','#1A2C48'],
-    sizeVariations:'10',
+    color:['#D08F5A','#2C2E32','#3E556D'],
+    sizeVariations:'5',
     connectParticles:'',
   });
 };
